1ae790821bec1315045653270dcf79be7e9f8cb0
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-08-11 17:10:21 +0300

    initial commit

---
+++ MobileGestursTesting/Scroll.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Service;
+
+namespace MobileGestursTesting
+{
+    public class Scroll
+    {
+        private AppiumDriver driver;
+        private AppiumLocalService service;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var androidOptions = new AppiumOptions
+            {
+                PlatformName = "Android",
+                AutomationName = "UiAutomator2",
+                DeviceName = "Pixel 7 API 34",
+                App = "C:\\Users\\Martin\\OneDrive - Software University (SoftUni)\\Documents\\QA\\09. Front-End Test Automation - July 2024\\11. Exercise Appium Mobile - Part 2\\ApiDemos-debug.apk",
+                PlatformVersion = "14"
+            };
+
+            service = new AppiumServiceBuilder()
+                .WithIPAddress("127.0.0.1")
+                .UsingPort(4723)
+                .Build();
+
+            driver = new AndroidDriver(service, androidOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+            service?.Dispose();
+        }
+
+        [Test]
+        public void ScrollTest()
+        {
+            var views = driver.FindElement(MobileBy.AccessibilityId("Views"));
+            views.Click();
+
+            ScrollToText("Lists");
+
+            var lists = driver.FindElement(MobileBy.AccessibilityId("Lists"));
+            Assert.That(lists, Is.Not.Null, "The 'Lists' element was not found after scrolling");
+            lists.Click();
+
+            var elementInLists = driver.FindElement(MobileBy.AccessibilityId("10. Single choice list"));
+            Assert.That(elementInLists, Is.Not.Null, "The expected element in the list was not found");
+        }
+
+        private void ScrollToText(string text)
+        {
+            driver.FindElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"" + text + "\"))"));
+        }
+    }
+}
---
+++ MobileGestursTesting/Swipe.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Interactions;
+
+namespace MobileGestursTesting
+{
+    internal class Swipe
+    {
+        private AppiumDriver driver;
+        private AppiumLocalService service;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var androidOptions = new AppiumOptions
+            {
+                PlatformName = "Android",
+                AutomationName = "UiAutomator2",
+                DeviceName = "Pixel 7 API 34",
+                App = "C:\\Users\\Martin\\OneDrive - Software University (SoftUni)\\Documents\\QA\\09. Front-End Test Automation - July 2024\\11. Exercise Appium Mobile - Part 2\\ApiDemos-debug.apk",
+                PlatformVersion = "14"
+            };
+
+            service = new AppiumServiceBuilder()
+                .WithIPAddress("127.0.0.1")
+                .UsingPort(4723)
+                .Build();
+
+            driver = new AndroidDriver(service, androidOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+            service?.Dispose();
+        }
+
+        [Test]
+        public void SwipeTest()
+        {
+            var views = driver.FindElement(MobileBy.AccessibilityId("Views"));
+            views.Click();
+
+            var gallery = driver.FindElement(MobileBy.AccessibilityId("Gallery"));
+            gallery.Click();
+
+            var photos = driver.FindElement(MobileBy.AccessibilityId("1. Photos"));
+            photos.Click();
+
+            var firstPhoto = driver.FindElement(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.widget.ImageView\").instance(0)"));
+
+            var action = new Actions(driver);
+            var swipe = action.ClickAndHold(firstPhoto)
+                              .MoveByOffset(-200, 0)
+                              .Release()
+                              .Build();
+            swipe.Perform();
+
+            var thirdPhoto = driver.FindElement(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.widget.ImageView\").instance(2)"));
+
+            Assert.That(thirdPhoto, Is.Not.Null, "The third picture was not found after swiping");
+        }
+    }
+}
